6223260ede34a9344237c7429b3a5664657d6949
HrushikeshKaranth <hrushikaranth@gmail.com>
2023-05-01 23:44:20 +0530

    .

--- my_portfolio/src/components/IntroSection.jsx
+++ my_portfolio/src/components/IntroSection.jsx
@@ -3,29 +3,31 @@ import { Icon } from '@iconify/react';
 function IntroSection() {
     return (
         <>
+            <div className='introMiddle'>
+                <div></div>
+            </div>
             <div className='introLeftSide'>
                 <div className='myName'>HRUSHIKESH H S</div>
-                <div className='jobProfile'>Web Developer</div>
-                <p className='bio'>A Creative Developer, Fast learner, Consistent worker 😇 with nearly a year
-                    experience in Full Stack Web Development.</p>
-            </div>
-            <div className='introMiddle'>
+                <div className="jobProfile">Web Developer</div>
+                <p className='bio'>A Creative Developer, Fast learner, Consistent worker with nearly a year
+                    experience 😇 in Full Stack Web Development.</p>
             </div>
             <div className='introRightSide'>
                 <div>
+                    <Icon className='icon' icon="logos:google-gmail" />
                     <span>hrushikeshkaranth@gmail.com</span>
-                    <Icon className='icon' icon="mdi:email-outline" />
                 </div>
                 <div>
-                    <span>Bengaluru, India</span><Icon className='icon' icon="mdi:location-outline" />
+                    <Icon className='icon' icon="logos:google-maps" />
+                    <span>Bengaluru, India</span>
                 </div>
                 <div>
-                    <a target={'_blank'} href='https://github.com/HrushikeshKaranth'>My GitHub</a>
-                    <Icon className='icon' icon="mdi:github" />
+                    <Icon className='icon' icon="mdi:github" style={{color:'white'}}/>
+                    <a target={'_blank'} href='https://github.com/HrushikeshKaranth'>My GitHub Profile</a>
                 </div>
                 <div>
+                    <Icon className='icon' icon="logos:linkedin-icon" />
                     <a target={'_blank'} href='https://www.linkedin.com/in/hrushikesh-h-s-25b78018b/'>My LinkedIn Profile</a>
-                    <Icon className='icon' icon="mdi:linkedin" />
                 </div>
             </div>
         </>
