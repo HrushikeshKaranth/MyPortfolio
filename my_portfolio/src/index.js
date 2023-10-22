1f84f7edde823cb17995de1548a5b91a55928b45
hrushikesh-religh <45314142+HrushikeshKaranth@users.noreply.github.com>
2023-10-22 13:28:48 +0530

    m

--- my_portfolio/src/index.js
+++ my_portfolio/src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <div style={
+    {
+      backgroundColor:'#303030',
+      height:'100vh'
+    }}>
     <App />
+  </div>
   </React.StrictMode>
 );
 
